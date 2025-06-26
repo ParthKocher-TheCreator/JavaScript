529daf84a1e9736e2ba26d6c4dec6014353be4b7
ParthKocher-TheCreator <parthkocher@gmail.com>
2025-06-26 17:42:39 +0530

    Data Types

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer veresion
+//alert() //we are using node js and not browser
+console.log(3)
+console.log(3
+    +
+3)//code readability should be there
+let name="Hitesh"
+let age=18
+let isLoggedIn=false
+/*
+ * Number: 2^53
+ * striing=""
+ * Boolean: True/False
+ * Null: Stand Alone value
+ * Undefined:
+ * symbol: unique
+ * Object
+ */
+console.log(typeof age)
