28ecd3a82e8a3deb5bdbe8d619b0c64a381042c1
ParthKocher-TheCreator <parthkocher@gmail.com>
2025-06-26 17:39:59 +0530

    variables

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=123
+let accountEmail="parthkocher@gmail.com"
+var accountPassword="123"
+accountCity="Mumbai"
+//accountId=2
+console.log(accountId)
+console.table([accountEmail,accountId,accountCity])
+//prefer not to use var because of issue with block scope and functional scope
