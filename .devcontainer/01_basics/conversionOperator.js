58e22039db90750953db73724c4f9c1bd5fec82a
ParthKocher-TheCreator <parthkocher@gmail.com>
2025-07-20 16:08:29 +0530

    basics 01

---
+++ .devcontainer/01_basics/conversionOperator.js
@@ -0,0 +1,6 @@
+let score="30"
+console.log(score)
+console.log(typeof(score))
+let valInNum=Number(score)
+console.log(valInNum)
+console.log(typeof(valInNum))
---
+++ .devcontainer/01_basics/date.js
@@ -0,0 +1,6 @@
+let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
